fc2eb774f0310bf5c896f2c4c651431928193e48
RajeshThakur1 <rajeshthakur1r@gmail.com>
2019-07-18 17:04:35 +0530

    first commit

---
+++ src/test/java/testPackage/TestOnly.java
@@ -0,0 +1,22 @@
+package testPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestOnly {
+	
+	@Test
+	public void testOnly() {
+		WebDriverManager.chromedriver()
+		.version("2.40")
+		.arch32()
+		.setup();
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.google.com/");
+		System.out.println("success");
+	}
+
+}
