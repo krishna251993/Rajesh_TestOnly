4dcb820a31c328f5b44df0471e1c0515b67658b5
RajeshThakur1 <rajeshthakur1r@gmail.com>
2019-07-18 18:02:27 +0530

    changes

--- src/test/java/testPackage/TestOnly.java
+++ src/test/java/testPackage/TestOnly.java
@@ -2,6 +2,7 @@ package testPackage;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -13,7 +14,13 @@ public class TestOnly {
 		WebDriverManager.chromedriver()
 		.version("2.40")
 		.setup();
-		WebDriver driver= new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("–no-sandbox");
+		options.addArguments("–disable-dev-shm-usage");
+		//options.setExperimentalOptions(“useAutomationExtension”, false);
+		options.setExperimentalOption("useAutomationExtension", false);
+
+		WebDriver driver= new ChromeDriver(options);
 		driver.get("https://www.google.com/");
 		System.out.println("success");
 	}
